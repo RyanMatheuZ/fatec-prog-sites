6f0326d17fb67aff4ec611804f1d8da92e129eff
Ryan Oliveira <oryan424@gmail.com>
2021-09-01 12:46:08 -0300

    Getting Started with Nuxt.js

---
+++ aula3-01-09/components/Menu.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="header">
+    <h1>Logo</h1>
+
+    <nav>
+      <ul>
+        <li v-for="item in menuItems" :key="item">{{ item }}</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        'Início',
+        'Produtos',
+        'Serviços',
+        'Contato'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  * {
+    font-family: sans-serif;
+  }
+
+  .header {
+    background-color: #F1F1F1;
+    width: 100%;
+  }
+
+  .header ul {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    list-style-type: none;
+  }
+
+  .header li {
+    cursor: pointer;
+    padding: 5px;
+  }
+</style>
---
+++ aula3-01-09/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Menu />
+
+    <p>Hello, {{ name }}!</p>
+
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  div {
+    color: #222;
+    font-size: 25px;
+    font-weight: 700;
+    text-align: center;
+  }
+</style>
